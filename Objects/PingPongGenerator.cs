92feb9f65455cfe44fbccc781cddf93dc2803b3b
simon sea413 <simonctemple@live.com>
2016-02-15 13:52:41 -0800

    fixed bugs

--- Objects/PingPongGenerator.cs
+++ Objects/PingPongGenerator.cs
@@ -2,65 +2,44 @@ using System.Collections.Generic;
 
 namespace PingPongGen
 {
-public class LeapYear
-{
-  public bool IsLeapYear (int year)
- {
-  return year % 4 == 0;
-}
-}
-}
-//   public class PingPong
-//   {
-//
-//     private int _id;
-//     private static List <PingPong> _instances = new List <PingPong> {};
-//
-//     public PingPong ()
-//     {
-//
-//       _instances.Add(this);
-//       _id = _instances.Count;
-//     }
-//
-//     // public int GetDaNumba()
-//     // {
-//     //   return _numba;
-//     // }
-//     public int GetId()
-//     {
-//       return _id;
-//     }
-//     public bool Return3(int inputnumber)
-//     {
-//       if (inputnumber == 3)
-//       {
-//         return true;
-//       }
-//     }
 
-    // public string PingPongMethod(int number)
-    // {
-    //   int userNumber = this.GetDaNumba();
-    //   List<string> changedValues = new List<string> {};
-    //   for (var i = 1; i <= userNumber; i++)
-    //   {
-    //     // var PingPongRestults = new PingPong;
-    //     // PingPongResults(i);
-    //     if (i % 15 == 0)
-    //     {
-    //       changedValues[i] = int.ToString("PingPong");
-    //     }
-    //     else if (i % 3 == 0)
-    //     {
-    //       changedValues[i] = int.ToString("Ping");
-    //     }
-    //     else if (i % 5 == 0)
-    //     {
-    //       changedValues[i] = int.ToString("Pong");
-    //     }
-    //     else
-    //     {
-    //       changedValues[i] = int.ToString(i);
-    //     }
-      // }
+  public class PingPong
+  {
+    private int _numba;
+    private int _id;
+    private static List<string> _instances = new List<string> {};
+
+    public PingPong(int numba)
+    {
+      _numba = numba;
+      for (int i = 1; i <= numba; i++)
+      {
+        if (i % 15 == 0)
+        {
+          _instances.Add("PingPong");
+        }
+        else if (i % 3 == 0)
+        {
+          _instances.Add("Ping");
+        }
+        else if (i % 5 == 0)
+        {
+          _instances.Add("Pong");
+        }
+        else
+        {
+          _instances.Add(i.ToString());
+        }
+      }
+    }
+        public List<string> GetAll()
+        {
+          return _instances;
+        }
+        public static void ClearAll()
+        {
+   _instances.Clear();
+        }
+
+  }
+}
--- Tests/PingPongGeneratorTest.cs
+++ Tests/PingPongGeneratorTest.cs
@@ -1,13 +1,30 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 namespace PingPongGen
 {
   public class PingPongTest
   {
-    [Fact]
-    public void IsLeapYear_ForNumberDivisibleByFour_true()
+    public class PPGTest
     {
-      LeapYear testLeapYear = new LeapYear();
-      Assert.Equal(true, testLeapYear.IsLeapYear(2012));
+      [Fact]
+      public void Test_Pingpong_Method1()
+      {
+        //Arrange
+        int numba = 7;
+        PingPong pg = new PingPong(numba);
+        List<string> test1 = pg.GetAll();
+
+        //Act
+        List<string> testList = new List<string>{"1", "2", "Ping", "4", "Pong", "Ping", "7"};
+
+        //Assert
+        Assert.Equal(testList, test1);
+      }
+      public void Dispose()
+      {
+  PingPong.ClearAll();
+      }
     }
-}
+  }
 }
